a81c02f0927242cc44bb4e979bb1dcb3f3e9bd61
Christopher Vachon <cvachon@prometrix.ca>
2024-04-19 21:20:46 -0400

    Add a Simple Code Page

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -60,6 +60,37 @@ export const BlueSkyIcon: React.FC<IIconProps> = ({ className = "" }) => (
     </svg>
 );
 
+export const EyeIcon: React.FC<IIconProps> = ({ className = "" }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className={new ClassNames(className).list()}
+    >
+        <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+        <path
+            fillRule="evenodd"
+            d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+            clipRule="evenodd"
+        />
+    </svg>
+);
+
+export const StarIcon: React.FC<IIconProps> = ({ className = "" }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className={new ClassNames(className).list()}
+    >
+        <path
+            fillRule="evenodd"
+            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+            clipRule="evenodd"
+        />
+    </svg>
+);
+
 const icons = {
     blueSky: BlueSkyIcon,
     twitter: TwitterIcon,
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,48 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface IGithubRepo {
+    name: string;
+    description: string;
+    sshUrl: string;
+    homepageUrl: string;
+    url: string;
+    forkCount: number;
+    stargazerCount: number;
+    watchers: {
+        totalCount: number;
+    };
+    languages: {
+        nodes: {
+            color: string;
+            name: string;
+        }[];
+    };
+}
+
+interface IGithubUser {
+    avatarUrl: string;
+    company?: string;
+    login: string;
+    url: string;
+    name: string;
+    pinnedItems: {
+        nodes: IGithubRepo[];
+    };
+}
+
+interface IGitHubRepoObjects {
+    id: string;
+    name: string;
+    url: string;
+    branch: {
+        entries: {
+            name: string;
+            type: string;
+            object: {
+                byteSize: number;
+                text: string;
+            };
+        }[];
+    };
+}
